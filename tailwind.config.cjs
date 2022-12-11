5beed38a3a75b9d1ff0ed0eb3dafc7c9819782a3
Lee Connelly <lee@leeconnelly.dev>
2022-12-11 19:03:10 +0000

    Add Ubuntu font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Ubuntu', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
